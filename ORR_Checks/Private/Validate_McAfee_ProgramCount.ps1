deb6ead385430dc708eee1902aa16d86c0efb440
Cody Parker <cparker01@textron.com>
2021-10-26 10:09:00 -0500

    updated McAfee to only check for the agent

--- ORR_Checks/Private/Validate_McAfee_ProgramCount.ps1
+++ ORR_Checks/Private/Validate_McAfee_ProgramCount.ps1
@@ -1,1 +1,1 @@
-Get-WmiObject -Class Win32_Product | select Name | where {$_.Name -like "*McAfee*"} | ConvertTo-Csv
+Get-WmiObject -Class Win32_Product | select Name | where {$_.Name -like "*McAfee Agent*"} | ConvertTo-Csv
